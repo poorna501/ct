ed6a23331fce2096f0afbca6bb522c4e8e145a78
poorna <poornab501@gmail.com>
2017-12-16 18:11:00 +0530

    16-DEC-2017

--- WebContent/secure/lang/ds1/js/kruskal.js
+++ WebContent/secure/lang/ds1/js/kruskal.js
@@ -1,9 +1,13 @@
 var MAX_VERTICES_SIZE = 8;
 var VERTICES_SIZE = index = SPANNINGTREE_SIZE = usedColorsCount = 0; 
 
-var kruskalArr= {};
+var kruskalArr= [];
 var edgesMap = {};
 var edgeWeight = {};
+var adjMap = {};
+
+
+var poornaMap = {};
 
 var VERTICES_FIXID_X_POS = [ 220, 145, 295, 70, 370, 145, 295, 220 ];
 var VERTICES_FIXID_Y_POS = [ 50, 150, 150, 250, 250, 350, 350, 450 ];
@@ -75,11 +79,15 @@ Kruskal.prototype.setup = function() {
 	this.verticesEdges = new Array(MAX_VERTICES_SIZE);
 	this.edgeWeight = new Array(MAX_VERTICES_SIZE * 5);
 	this.edgeLine = new Array(MAX_VERTICES_SIZE * 5);
+	this.edgeRect = new Array(MAX_VERTICES_SIZE * 5);
+	this.WeightRect = new Array(MAX_VERTICES_SIZE * 5);
 	
 	for (var i = 0; i < MAX_VERTICES_SIZE * 5; i++) {
 		this.vertices[i] = this.nextIndex++;
 		this.edgeLine[i] = this.nextIndex++;
 		this.edgeWeight[i] = this.nextIndex++;
+		this.edgeRect[i] = this.nextIndex++;
+		this.WeightRect[i] = this.nextIndex++;
 	}
 	
 	this.ADJACENT_TABLE_HORIZONTAL_LINE = this.nextIndex++;
@@ -154,85 +162,77 @@ Kruskal.prototype.disableUI = function(event) {
 
 Kruskal.prototype.vertex = function() {
 	this.commands = new Array();
-	
 	this.cmd("CreateCircle", this.vertices[VERTICES_SIZE], VERTICES_SIZE, VERTICES_FIXID_X_POS[VERTICES_SIZE], VERTICES_FIXID_Y_POS[VERTICES_SIZE]);
 	$("#fromID ul").append("<li><a href='#'>" + VERTICES_SIZE + "</a></li>");
 	$("#toID ul").append("<li><a href='#'>" + VERTICES_SIZE + "</a></li>");
 	addVertext = true;
 	SPANNINGTREE_SIZE++;
 	VERTICES_SIZE++;
-	
 	return this.commands;
 }
  
 Kruskal.prototype.edge = function() {
 	this.commands = new Array();
-	
 	this.weightRect = new Array();
 	this.edgeRect = new Array();
-	
 	for (var i = 0; i < MAX_VERTICES_SIZE * 5; i++) {
 		this.weightRect[i] = this.nextIndex++;
 		this.edgeRect[i] = this.nextIndex++;
 	}
-	
 	fromEdgeAndToEdgeValues();
-	if (edgesMap[fromEdge + "-" + toEdge] == undefined) {
+	if (edgesMap[fromEdge + " - " + toEdge] == undefined) {
 		if (fromEdge == toEdge) {
 			alertify.alert("Same vertices, please give different vertices!");
 		} else {
 			this.setCirclehighlight();
 			this.connect_vertices();
 			this.cmd("Step");
-			this.cmd("Step");
 			this.removeCircleColor();
-			edgeWeight[fromEdge + "-" + toEdge] = $("#edgeWeight").val();
-			edgeWeight[toEdge + "-" + fromEdge] = $("#edgeWeight").val();
-			edgesMap[fromEdge + "-" + toEdge] = true;
-			edgesMap[toEdge + "-" + fromEdge] = true;
+			edgeWeight[fromEdge + " - " + toEdge] = $("#edgeWeight").val();
+			kruskalArr.push({key: fromEdge + " - " + toEdge, val: $("#edgeWeight").val()});
+			edgesMap[fromEdge + " - " + toEdge] = true;
 			index++;
 		}
 	} else {
 		var flag = false; 
-		console.log("in DisCon before " + this.vertices);
-		
 		display_Prompt();
 	}
-		
 	return this.commands;
 }
 
 function display_Prompt() {
 	alertify.confirm("The <r>edge</r> between <r>"+ $("#fromID .active").text() +"</r> and <r>"+ $("#toID .active").text() 
-			+ "</r> is already exists. <br/>Do u Want to <r>chage the weight of the Edge</r>.", function() {
-	$("#alertify-ok").text("Yes");
-	$("#alertify-text").attr("maxlength", "3");
-	$(".alertify").css({"left": "45%", "top": "50px"});
-	$("#testBtn").click();
-	});
+			+ "</r> is already exists. <br/>Do u Want to <r>chage the weight of the Edge</r>.");
+		$("#alertify-ok").text("Yes");
+		$("#alertify-text").attr("maxlength", "3");
+		$(".alertify").css({"left": "45%", "top": "50px"});
+		$("#alertify-ok").click(function() {
+			$("#testBtn").click();
+		});
 }
 
 Kruskal.prototype.testing = function() {
 	this.commands = new Array();
-	
 	fromEdgeAndToEdgeValues();
 	this.cmd("Step");
 	this.setCirclehighlight()
 	this.cmd("SetEdgeColor", this.vertices[fromEdge], this.vertices[toEdge], "#e62e00");
-	this.cmd("Step");
-	this.cmd("SETEDGEALPHA", this.vertices[fromEdge], this.vertices[toEdge], "#e62e00");
-	this.cmd("Step");
 	this.cmd("DisConnect", this.vertices[fromEdge], this.vertices[toEdge], "#000000", 0.4, false, "", 0, true);
 	this.connect_vertices();
+	edgeWeight[fromEdge + " - " + toEdge] = $("#edgeWeight").val();
+	for (let i = 0; i < Object.keys(edgeWeight).length; i++) {
+		if (kruskalArr[i].key == fromEdge + " - " + toEdge) {
+			kruskalArr[i].val = $("#edgeWeight").val();
+		}
+	}
 	this.removeCircleColor();
-	
 	return this.commands;
 }
 
 function fromEdgeAndToEdgeValues() {
 	fromEdge = parseInt($("#fromID .active").text());
 	toEdge = parseInt($("#toID .active").text());
-	key = fromEdge + "-" + toEdge;
+	key = fromEdge + " - " + toEdge;
 	var val1 = (fromEdge < toEdge) ? fromEdge : toEdge; 
 	var val2 = (fromEdge > toEdge) ? fromEdge : toEdge;
 	fromEdge = val1;
@@ -249,9 +249,9 @@ Kruskal.prototype.setCirclehighlight = function() {
 }
 
 Kruskal.prototype.connect_vertices = function() {
-	if ((key == "0-3" || key == "3-0") || (key == "3-7" || key == "7-3")) {
+	if ((key == "0 - 3" || key == "3 - 0") || (key == "3 - 7" || key == "7 - 3")) {
 		this.cmd("connect", this.vertices[fromEdge], this.vertices[toEdge], "#000000", 0.4, false, $("#edgeWeight").val(), 0, true);
-	} else if ((key == "0-4" || key == "4-0") || (key == "4-7" || key == "7-4")) {
+	} else if ((key == "0 - 4" || key == "4 - 0") || (key == "4 - 7" || key == "7 - 4")) {
 		this.cmd("connect", this.vertices[fromEdge], this.vertices[toEdge], "#000000", -0.4, false, $("#edgeWeight").val(), 0, true);
 	} else {
 		this.cmd("connect", this.vertices[fromEdge], this.vertices[toEdge], "#000000", 0, false, $("#edgeWeight").val(), 0, true);
@@ -267,20 +267,72 @@ Kruskal.prototype.removeCircleColor = function() {
 
 Kruskal.prototype.start = function() {
 	this.commands = new Array();
-	console.log("Hello Start Btn");
-	this.cmd("CreateLabel", this.nextIndex++, "Sort the Edge", 475, 40);
-	this.cmd("CreateRectangle", this.nextIndex++, "Edge", 40, 25, 450, 60);
-	this.cmd("CreateRectangle", this.nextIndex++, "Weight", 40, 25, 491, 60);
-	
-	
-	
-	
-	
-	
+	this.cmd("Step");
+	this.sortTheEdges();
+	edgeWeight[Object.keys(edgeWeight)[0]];
 	return this.commands;
 }
 
+Kruskal.prototype.sortTheEdges = function() {
+	this.commands = new Array();
+	this.cmd("CreateLabel", this.nextIndex++, "Order the Edge", 475, 40);
+	this.cmd("CreateRectangle", this.nextIndex++, "Edge", 40, 25, 450, 60);
+	this.cmd("CreateRectangle", this.nextIndex++, "Weight", 40, 25, 491, 60);
+	this.cmd("SetBackgroundColor", this.nextIndex - 2, colorsArr[usedColorsCount]);
+	this.cmd("SetBackgroundColor", this.nextIndex - 1, colorsArr[usedColorsCount]);
+	this.sortEdgeLogic();
+	this.cmd("Step");
+	return this.commands;
+}
 
+Kruskal.prototype.sortEdgeLogic = function() {
+	kruskalArr.sort(function(a, b) { return a.val - b.val});
+	for (var i = 0; i < kruskalArr.length; i++) {
+		var val = kruskalArr[i].key.split(" - ");
+		fromEdge = parseInt(val[0]);
+		toEdge = parseInt(val[1]);
+		this.setCirclehighlight();
+		
+		this.cmd("SetEdgeColor", this.vertices[fromEdge], this.vertices[toEdge], colorsArr[usedColorsCount]);
+		this.cmd("SETEDGEHIGHLIGHT", this.vertices[fromEdge], this.vertices[toEdge], colorsArr[usedColorsCount + 1]);
+		this.cmd("Step");
+		this.cmd("CreateRectangle", this.edgeRect[i], kruskalArr[i].key, 40, 25, 450, 85 + (i * 20));
+		this.cmd("CreateRectangle", this.WeightRect[i], kruskalArr[i].val, 40, 25, 491, 85 + (i * 20));
+		this.cmd("SetBackgroundColor", this.edgeRect[i], colorsArr[usedColorsCount + 1]);
+		this.cmd("SetBackgroundColor",this.WeightRect[i], colorsArr[usedColorsCount + 1]);
+		this.cmd("SetHighlight", this.edgeRect[i], colorsArr[usedColorsCount + 1]);
+		this.cmd("SetHighlight", this.WeightRect[i], colorsArr[usedColorsCount + 1]);
+		this.cmd("SetEdgeColor", this.vertices[fromEdge], this.vertices[toEdge], "");
+		this.cmd("SETEDGEHIGHLIGHT", this.vertices[fromEdge], this.vertices[toEdge], "");
+		this.cmd("Step");
+		this.cmd("SetHighlight", this.edgeRect[i], "");
+		this.cmd("SetHighlight", this.WeightRect[i], "");
+		this.cmd("Step");
+		this.removeCircleColor();
+		this.cmd("SetHighlight", this.vertices[fromEdge], "");
+		this.cmd("SetHighlight", this.vertices[toEdge], "");
+		this.cmd("Step");
+		this.cmd("Step");
+		this.drawMinSpanningTree();
+	}
+	this.cmd("Step");
+}
+
+Kruskal.prototype.drawMinSpanningTree = function() {
+	console.log("In min Spanning Tree");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
 
 
 
